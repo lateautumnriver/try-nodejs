4e6ab0303596996ba05770a846f36ffac7c04780
laur <sunnysummer.sk8@gmail.com>
2024-11-21 11:43:13 +0900

    added setup.sh to prepare for running some code.

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# Opt-out collecting Next.js telemetry
+# See https://nextjs.org/telemetry
+# No collection, just output
+export NEXT_TELEMETRY_DEBUG=1
+
+# No collection by an environment variable
+export NEXT_TELEMETRY_DISABLED=1
+
+# No collection eternally
+# npx next telemetry disable
+
+# Check the status of telemetry collection
+# npx next telemetry status
